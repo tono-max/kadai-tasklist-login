c2d046f9822ce956c5d76c50039373e9f2c4834c
tono <tonton.kirawaku@gmail.com>
2020-08-15 11:35:10 +0000

    nosettask

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,7 @@ class TasksController < ApplicationController
   
   before_action :require_user_logged_in
   before_action :correct_user,only: [:show, :edit, :update, :destroy]
-  before_action :set_task,only: [:show, :edit, :update, :destroy]
-  
-  
+ 
   def index
     @tasks = current_user.tasks.all
   end
@@ -49,10 +47,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
